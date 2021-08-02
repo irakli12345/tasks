2d197069a947951e923401f41b404c1369a16c5b
Irakli <irakli@tchigl.com>
2021-08-02 12:39:44 +0400

    made changes to app, header and css file

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({title}) => {
+    const onClick = (e) => {
+        console.log(e)
+    }
+    return (
+        <header className="header">          
+        <h2>Task Tracker</h2>
+        <Button color="green" text="Hello" onClick={onClick}></Button>       
+</header>
+        
+    )
+}
+Header.defaultProps = {
+    title: 'Good Morning America!'
+}
+Header.propTypes = {
+    title: PropTypes.string.isRequired
+}
+//const headerStyle = {
+//    color: 'blue',
+//    backgroundColor: 'yellow'
+//}
+export default Header
