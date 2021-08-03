cf24c4f639faa0f1f66e80c5a3f9655c85952d6a
Irakli <irakli@tchigl.com>
2021-08-03 13:55:13 +0400

    added async loading of data from a server

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 function App() {
   const [tasks, setTasks] = useState([]);
   const [showAddTask, setShowAddTask] = useState(false);
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+    getTasks();
+  }, []);
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:5000/tasks");
+    const data = await res.json();
+    return data;
+  };
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
