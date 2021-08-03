96256a805300a93f82119bbb2a2fba4e3d6714fc
Irakli <irakli@tchigl.com>
2021-08-03 13:30:03 +0400

    added json-server and created database

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 import { useState } from "react";
 function App() {
-  const [tasks, setTasks] = useState([
-    { text: "Bugidibu", id: 1, date: "2021-08-02", reminder: true },
-    { text: "Ha ha yu dead", id: 2, date: "2021-08-03", reminder: true },
-    { text: "Cocoa Latte", id: 3, date: "2021-08-03", reminder: true },
-    { text: "Yass queen", id: 4, date: "2021-08-04", reminder: true },
-  ]);
+  const [tasks, setTasks] = useState([]);
   const [showAddTask, setShowAddTask] = useState(false);
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
